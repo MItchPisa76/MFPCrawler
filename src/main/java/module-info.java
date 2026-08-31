0176a82756ba40478385037b87d58a0acaa1fd86
MItchPisa76 <89701539+MItchPisa76@users.noreply.github.com>
2026-08-31 13:47:54 +0200

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,13 @@
-module com.mfpcrawler {
+module MFPCrawler {
     requires java.base;
 	requires java.desktop;
 	requires java.net.http;
 	requires java.sql;
 	requires org.snmp4j;
 	requires com.fasterxml.jackson.databind;
+	
+    opens mtc.MFPCrawler to com.fasterxml.jackson.databind;
+	
     // Add any other required modules here
     // For example, if using JavaFX:
     // requires javafx.controls;
